08673758740d0971d71a8bbb5006cd5ea3dd738b
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-03-21 10:56:11 -0300

    feat: add CRUD views to actors

--- actors/views.py
+++ actors/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import generics
+from actors.models import Actor
+from .serializers import ActorSerializer
 
-# Create your views here.
+class ActorCreateListView(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    
+class ActorRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    
+    def delete(self, *args, **kwargs):
+        instance = self.get_object()
+        
+        try:
+            instance.delete()
+            return JsonResponse({'message': 'Actor removed sucessfully'}, status=204)
+        
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDeleteView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-retrieve-update-destroy'),
+    
+    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>', ActorRetrieveUpdateDeleteView.as_view(), name='actor-retrieve-update-destroy')
 ]
