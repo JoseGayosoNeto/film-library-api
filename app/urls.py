d9d7c8083b299b8c0bee4830b0137bbe2750ac8c
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-03-25 20:10:30 -0300

    feat: add CRUD views to reviews

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDeleteView
 from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 
 urlpatterns = [
@@ -16,4 +17,7 @@ urlpatterns = [
     
     path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
     path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-retrieve-update-destroy'),
+    
+    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
+    path('reviews/<int:pk>', ReviewRetrieveUpdateDestroyView.as_view(), name='review-retrieve-update-destroy')
 ]
