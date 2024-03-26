976494253554aebd1f0bd2ce7d97e3025302f13b
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-03-26 11:13:11 -0300

    feat: add validation and rate field to movies serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,29 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
     
     class Meta:
         model = Movie
         fields = '__all__'
+    
+    def get_rate(self, obj):        
+        
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        
+        if rate:
+            return f'{rate:.1f}/5.0'
+        
+        return None
+    
+    def validate_release_date(self, value):
+        if value.year < 1888:
+            raise serializers.ValidationError('Release date cannot be less than 1888.')
+        return value
+    
+    def validate_resume(self,value):
+        if len(value) > 500:
+            raise serializers.ValidationError('Resume must not be longer than 200 characters.')
+        return value
