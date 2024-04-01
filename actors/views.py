02dfd0d8106e8d458ab3b9b2e5f8105b661003fc
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-04-01 10:21:55 -0300

    feat: add JWT authentication

--- actors/views.py
+++ actors/views.py
@@ -1,13 +1,15 @@
 from django.http import JsonResponse
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from actors.models import Actor
 from .serializers import ActorSerializer
-
 class ActorCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
     
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
     
--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
-from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('api/v1/', include('authentication.urls')),
     
     path('api/v1/', include('genres.urls')),
     path('api/v1/', include('actors.urls')),
