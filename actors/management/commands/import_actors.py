10a9b697446f57ab82a6e453d180a6482731f2db
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-04-25 10:43:31 -0300

    chore: apply style checking

--- actors/management/commands/import_actors.py
+++ actors/management/commands/import_actors.py
@@ -5,29 +5,29 @@ from django.core.management.base import BaseCommand, CommandParser
 
 
 class Command(BaseCommand):
-    
+
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument(
             'filename',
             type=str,
             help='Name of the CSV file containing the actors to be inserted'
         )
-    
+
     def handle(self, *args, **options):
         filename = options['filename']
 
         with open(filename, 'r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
-            
+
             for row in reader:
                 name = row['name']
                 birthday = datetime.strptime(row['birthday'], '%Y-%m-%d').date()
                 nationality = row['nationality']
-                
+
                 Actor.objects.create(
                     name=name,
                     date_of_birth=birthday,
                     nationality=nationality
                 )
-    
+
         self.stdout.write(self.style.SUCCESS('Actors added to database sucessfully'))
